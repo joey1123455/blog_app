f1af3fbdb55c6b8af7c4bce78d350d5b5cca1b41
joey <folayanjoey@gmail.com>
2023-02-04 21:26:34 +0100

    minor fix

--- blog/views.py
+++ blog/views.py
@@ -86,8 +86,7 @@ def post_share(request, post_id):
 
     return render(request, 'blog\post\share.html', {'post': post, 
                                                     'form': form, 
-                                                    'sent': sent}
-                                                    )
+                                                    'sent': sent})
 
 
 @require_POST
