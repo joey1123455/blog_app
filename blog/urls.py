f88d2cd674bb4ed5ed18ec74c7f03faec6441453
joey <folayanjoey@gmail.com>
2023-02-04 17:29:07 +0100

    RSS feed enabled

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from . import views 
+from . feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -10,4 +12,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post-detail'),
     path('<int:post_id>/share/', views.post_share, name='post-share'),
     path('<int:post_id>/comment/', views.post_comment, name='post-comment'),
+    path('feed/', LatestPostsFeed(), name='post-feed'),
 ]
