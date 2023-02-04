64f4b54707de32b5489dc2245ddf7e78a564d8f8
joey <folayanjoey@gmail.com>
2023-02-04 10:19:59 +0100

    Enabled similar posts by tags

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 from taggit.models import Tag
 
 from .models import *
@@ -38,6 +39,7 @@ class PostLists(ListView):
 
 
 def post_detail(request, year, month, day, post):
+
     post = get_object_or_404(
         Post, 
         status=Post.Status.PUBLISHED,
@@ -48,7 +50,11 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    context = {'form': form, 'post': post, 'comments': comments}
+    post_tags_ids =Post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    context = {'form': form, 'post': post, 'comments': comments, 'similar_posts': similar_posts}
     return render(request, 'blog\post\detail.html', context)
 
 
